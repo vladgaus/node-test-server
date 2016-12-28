0d91a125587f92be67a686e5a8d6da8c6a301a21
Владимир Синика <vladimirsinika@MacBook-Pro-Vladimir.local>
2016-12-28 20:00:42 +0200

    Add project page

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send('Error page');
 });
